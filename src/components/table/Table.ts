d32162068d74d1fa1a62c04b58db09875214cd96
fogel <fogel_92@bk.ru>
2023-01-26 00:45:46 +0700

    feat: optimize app, resizer styles

--- src/components/table/Table.ts
+++ src/components/table/Table.ts
@@ -16,24 +16,43 @@ export class Table extends ExcelComponent {
       const $parent = $resizer.closest('[data-type="resizable"]');
       const coords = $parent.getCoords();
       const type = event.target.dataset.resize;
+      const sideProp = type === 'col' ? 'bottom' : 'right';
+      let value: number;
+      $resizer.css({
+        opacity: 1,
+        [sideProp]: '-5000px',
+      });
       const cells = this.$root instanceof Dom ? this.$root.findAll(`[data-col="${$parent.data.col}"]`) : '';
 
       document.onmousemove = (event) => {
         if (type === 'col') {
           const delta = Math.floor(event.pageX - coords.right);
-          const value = coords.width + delta;
-          $parent.css({width: value + 'px'});
-          // @ts-ignore
-          cells.forEach((el) => el.style.width = value + 'px');
+          $resizer.css({right: -delta + 'px'});
+          value = coords.width + delta;
         } else {
           const delta = Math.floor(event.pageY - coords.bottom);
-          const value = coords.height + delta;
-          $parent.css({height: value + 'px'});
+          value = coords.height + delta;
+          $resizer.css({bottom: -delta + 'px'});
         }
       };
 
       document.onmouseup = (event) => {
         document.onmousemove = null;
+        document.onmouseup = null;
+
+        if (type === 'col') {
+          $parent.css({width: value + 'px'});
+          // @ts-ignore
+          cells.forEach((el) => el.style.width = value + 'px');
+        } else {
+          $parent.css({height: value + 'px'});
+        }
+
+        $resizer.css({
+          opacity: 0,
+          bottom: 0,
+          right: 0,
+        });
       };
     }
   }
